051f3f8ef508181700c78607615f09b6044b3eb5
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-31 08:56:36 +0530

    Refactored the code to cache the PreparedStatement and create a singleton

--- src/main/java/PayRoll.java
+++ src/main/java/PayRoll.java
@@ -2,7 +2,19 @@ import java.sql.*;
 import java.util.*;
 
 public class PayRoll {
+    private PayRoll() {
+    }
+    private static PayRoll pinstance=null;
+    public static PayRoll getInstance()
+    {
+    if(pinstance==null)
+        pinstance=new PayRoll();
+
+    return pinstance;
+    }
+
     ConnectionRetriever con=new ConnectionRetriever();
+    PreparedStatement payrollUpdateStatement;
     public  List<Employee> readData(){
         String sql="select * from employee e, payroll p where e.emp_id=p.emp_id ;";
         List<Employee> arr=new ArrayList<Employee>();
@@ -33,10 +45,10 @@ public class PayRoll {
         try {
             String sql = "update payroll set basic_pay= " + salary + " where emp_id=" + "(select emp_id from employee where name = '" + name + "');";
             Connection c = con.getConnection();
-            PreparedStatement statement=c.prepareStatement("update payroll set basic_pay= ? where emp_id=(select emp_id from employee where name = ? name);");
-            statement.setInt(1,salary);
-            statement.setString(2,name);
-           statement.executeUpdate(sql);
+            payrollUpdateStatement=c.prepareStatement("update payroll set basic_pay= ? where emp_id=(select emp_id from employee where name = ? name);");
+            payrollUpdateStatement.setInt(1,salary);
+            payrollUpdateStatement.setString(2,name);
+            payrollUpdateStatement.executeUpdate(sql);
         }
         catch (Exception e)
         {
--- src/test/java/TestPayroll.java
+++ src/test/java/TestPayroll.java
@@ -2,16 +2,18 @@ import org.junit.*;
 
 public class TestPayroll {
 
+    PayRoll p;
+
     @Test
     public void test1CountEntriesAfterReadingData()
     {
-        PayRoll p=new PayRoll();
+        p=PayRoll.getInstance();
         Assert.assertEquals(3,p.readData().size());
     }
     @Test
     public void test2CheckUpdate()
     {
-        PayRoll p=new PayRoll();
+        p=PayRoll.getInstance();
         p.update("a",300000);
         Assert.assertEquals(300000,p.readData().get(0).basic_pay);
     }
